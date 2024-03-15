aa4f39bc0e7e864bf2ee88033eac9cb2b36eebd6
carlosinti_94@hotmail.com <carlosinti_94@hotmail.com>
2024-03-15 12:00:21 +0100

    engine vector functions

--- srcs/engine/vector/vector2/matrix2_utils.c
+++ srcs/engine/vector/vector2/matrix2_utils.c
@@ -6,16 +6,16 @@
 /*   By: cagonzal <cagonzal@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 12:43:03 by cagonzal          #+#    #+#             */
-/*   Updated: 2024/03/15 08:47:41 by cagonzal         ###   ########.fr       */
+/*   Updated: 2024/03/15 11:59:15 by cagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <vector3.h>
+#include <vector2.h>
 
 
-t_matrix3   ft_matrix3(void)
+t_matrix2   ft_matrix2(void)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = 0.0f;
 	m.elements[0][1] = 0.0f;
@@ -30,9 +30,9 @@ t_matrix3   ft_matrix3(void)
 	return (m);
 }
 
-t_matrix3   ft_matr3add(t_matrix3 m1, t_matrix3 m2)
+t_matrix2   ft_matr3add(t_matrix2 m1, t_matrix2 m2)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = m1.elements[0][0] + m2.elements[0][0];
 	m.elements[0][1] = m1.elements[0][1] + m2.elements[0][1];
@@ -47,9 +47,9 @@ t_matrix3   ft_matr3add(t_matrix3 m1, t_matrix3 m2)
 	return (m);
 }
 
-t_matrix3   ft_matr3sub(t_matrix3 m1, t_matrix3 m2)
+t_matrix2   ft_matr3sub(t_matrix2 m1, t_matrix2 m2)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = m1.elements[0][0] - m2.elements[0][0];
 	m.elements[0][1] = m1.elements[0][1] - m2.elements[0][1];
@@ -61,9 +61,9 @@ t_matrix3   ft_matr3sub(t_matrix3 m1, t_matrix3 m2)
 	return (m);
 }
 
-t_matrix3   ft_matr3mul(t_matrix3 m1, t_matrix3 m2)
+t_matrix2   ft_matr3mul(t_matrix2 m1, t_matrix2 m2)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = m1.elements[0][0] * m2.elements[0][0];
 	m.elements[0][1] = m1.elements[0][1] * m2.elements[0][1];
@@ -78,9 +78,9 @@ t_matrix3   ft_matr3mul(t_matrix3 m1, t_matrix3 m2)
 	return (m);
 }
 
-t_matrix3   ft_matr3fmul(t_matrix3 m1, float f)
+t_matrix2   ft_matr3fmul(t_matrix2 m1, float f)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = m1.elements[0][0] * f;
 	m.elements[0][1] = m1.elements[0][1] * f;
@@ -96,9 +96,9 @@ t_matrix3   ft_matr3fmul(t_matrix3 m1, float f)
 }
 
 
-t_matrix3   ft_matr3div(t_matrix3 m1, t_matrix3 m2)
+t_matrix2   ft_matr3div(t_matrix2 m1, t_matrix2 m2)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = m1.elements[0][0] / m2.elements[0][0];
 	m.elements[0][1] = m1.elements[0][1] / m2.elements[0][1];
@@ -113,9 +113,9 @@ t_matrix3   ft_matr3div(t_matrix3 m1, t_matrix3 m2)
 	return (m);
 }
 
-t_matrix3   ft_matr3fdiv(t_matrix3 m1, float f)
+t_matrix2   ft_matr3fdiv(t_matrix2 m1, float f)
 {
-    t_matrix3 m;
+    t_matrix2 m;
 
     m.elements[0][0] = m1.elements[0][0] / f;
 	m.elements[0][1] = m1.elements[0][1] / f;
