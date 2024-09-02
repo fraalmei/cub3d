1618196c429978deff56fc611b945d1e96d35a62
fraalmei <Paco_kauka@hotmail.com>
2024-09-02 13:26:10 +0200

    check: mostrar imagenes y comprobador de teclas

--- srcs/paco_engine/engine.c
+++ srcs/paco_engine/engine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   engine.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fraalmei <fraalmei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: p <p@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 13:57:18 by fraalmei          #+#    #+#             */
-/*   Updated: 2024/09/01 17:49:16 by fraalmei         ###   ########.fr       */
+/*   Updated: 2024/09/02 13:23:33 by p                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ static int	end_check_program(t_game *game)
 {
 	if (game->window.img)
 		mlx_delete_image(game->window.mlx, game->window.img);
-	ft_printf_fd(1, "Pulsado ESC\n");
 	mlx_close_window(game->window.mlx);
 	mlx_terminate(game->window.mlx);
 	free_data(game);
@@ -29,7 +28,6 @@ static int	change_img(t_game *game)
 
 	if (i > 3 || i < 0)
 		i = 0;
-	ft_printf_fd(1, "Cambia de imagen %d\n", i);
 	wait(1000);
 	if (game->window.img)
 		mlx_delete_image(game->window.mlx, game->window.img);
@@ -37,7 +35,6 @@ static int	change_img(t_game *game)
 		game->window.img = mlx_texture_to_image(game->window.mlx, game->map->map_textures[i]->png_img);
 	if (mlx_image_to_window(game->window.mlx, game->window.img, 0, 0) < 0)
 		end_check_program(game);
-	ft_printf_fd(1, "Muestra imagen %d\n", i);
 	i++;
 	return (0);
 }
@@ -48,50 +45,32 @@ static t_game	*init_check(t_game *game)
 	if (game->window.mlx == NULL)
 		end_check_program(game);
 	change_img(game);
-	//game->window.mlx->window = mlx_new_window(game->window.mlx, S_WIDTH, S_HEIGHT, "Cub3D");
-	//game->frame.image = mlx_new_image(game->window.mlx, S_WIDTH, S_HEIGHT);
 	return (game);
 }
 
-static int	read_keys_check(void *param)
+static int	read_keys_check(mlx_key_data_t keydata, void *param)
 {
 	t_game			*data;
-
+	char			*str;
 
 	ft_printf_fd(1, "Pulsa tecla\n");
 	data = (t_game *)param;
-	//if (key_pressed == K_ESC || !data->window.mlx)
-	if (mlx_is_key_down(data->window.mlx, K_ESC) || !data->window.mlx)
-		(ft_printf_fd(1, "Pulsado ESC\n"), end_check_program(data));
-	if (mlx_is_key_down(data->window.mlx, K_SPACE))
-		(ft_printf_fd(1, "Pulsado ESP\n"), change_img(data));
-	return (0);
-}
-
-static int	check_update(void *param)
-{
-	t_game			*game;
-
-	ft_printf_fd(1, "Pulsa tecla\n");
-	game = (t_game *)param;
-/* 	ft_printf_fd(1, "Actualiza cambio imagen\n");
-	if (game->window.img)
-		mlx_delete_image(game->window.mlx, game->window.img);
-	//hook(game->window, 0, 0);
-	// cast_rays(game);
-	change_img(game);
-	ft_printf_fd(1, "Cambió imagen\n"); */
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == 1)
+		(ft_printf_fd(1, "Pulsado tecla %d\n", keydata.key), end_check_program(data));
+	if ((keydata.key >= 0)&& keydata.action == 1)
+	{
+		str = (char *)ft_calloc(sizeof(char), 100);
+		sprintf(str, "Pulsado tecla %d\n", keydata.key);
+		(ft_printf_fd(1, "Pulsado tecla %d\n", keydata.key), change_img(data));
+		mlx_put_string(data->window.mlx, str, 0, 0);
+		free (str);
+	}
 	return (0);
 }
 
 void	engine_check(t_game *game)
 {
-	xpm_t	*swap;
-
 	game = init_check(game);
-	//mlx_loop_hook(game->window.mlx, 17, 0, &end_check_program, &game->window);
-	mlx_key_hook(game->window.mlx, read_keys_check, game); // key press and release
-	//mlx_loop_hook(game->window.mlx, check_update, game); // game loop continuously call a specified function to update the game state and render the frames.
-
-	mlx_loop(game->window.mlx); // mlx loop
+	mlx_key_hook(game->window.mlx, read_keys_check, game);
+	mlx_loop(game->window.mlx);
 }
