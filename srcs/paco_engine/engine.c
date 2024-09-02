395d6f1d368959560b131e95c84b93af71f69dc1
fraalmei <Paco_kauka@hotmail.com>
2024-09-02 15:44:56 +0200

    agregado raton

--- srcs/paco_engine/engine.c
+++ srcs/paco_engine/engine.c
@@ -6,7 +6,7 @@
 /*   By: p <p@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 13:57:18 by fraalmei          #+#    #+#             */
-/*   Updated: 2024/09/02 13:23:33 by p                ###   ########.fr       */
+/*   Updated: 2024/09/02 15:44:18 by p                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ static int	change_img(t_game *game)
 
 	if (i > 3 || i < 0)
 		i = 0;
-	wait(1000);
 	if (game->window.img)
 		mlx_delete_image(game->window.mlx, game->window.img);
 	if (game->map->map_textures[i]->png_img != NULL)
@@ -48,7 +47,7 @@ static t_game	*init_check(t_game *game)
 	return (game);
 }
 
-static int	read_keys_check(mlx_key_data_t keydata, void *param)
+static void	read_keys_check(mlx_key_data_t keydata, void *param)
 {
 	t_game			*data;
 	char			*str;
@@ -56,21 +55,39 @@ static int	read_keys_check(mlx_key_data_t keydata, void *param)
 	ft_printf_fd(1, "Pulsa tecla\n");
 	data = (t_game *)param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == 1)
-		(ft_printf_fd(1, "Pulsado tecla %d\n", keydata.key), end_check_program(data));
+		(ft_printf_fd(1, "Pulsado tecla teclado %d\n", keydata.key), end_check_program(data));
 	if ((keydata.key >= 0)&& keydata.action == 1)
 	{
 		str = (char *)ft_calloc(sizeof(char), 100);
-		sprintf(str, "Pulsado tecla %d\n", keydata.key);
-		(ft_printf_fd(1, "Pulsado tecla %d\n", keydata.key), change_img(data));
+		sprintf(str, "Pulsado tecla teclado %d\n", keydata.key);
+		(ft_printf_fd(1, str), change_img(data));
 		mlx_put_string(data->window.mlx, str, 0, 0);
 		free (str);
 	}
-	return (0);
+}
+
+static void	read_mouse_check(mouse_key_t button, action_t action, modifier_key_t mods, void *param)
+{
+	t_game			*data;
+	char			*str;
+
+	ft_printf_fd(1, "Pulsa tecla\n");
+	data = (t_game *)param;
+	(void) mods;
+	if ((button >= 0)&& action == 1)
+	{
+		str = (char *)ft_calloc(sizeof(char), 100);
+		sprintf(str, "Pulsado tecla raton %d\n", button);
+		(ft_printf_fd(1, str), change_img(data));
+		mlx_put_string(data->window.mlx, str, 0, 0);
+		free (str);
+	}
 }
 
 void	engine_check(t_game *game)
 {
 	game = init_check(game);
-	mlx_key_hook(game->window.mlx, read_keys_check, game);
+	mlx_key_hook(game->window.mlx, &read_keys_check, game);
+	mlx_mouse_hook(game->window.mlx, &read_mouse_check, game);
 	mlx_loop(game->window.mlx);
 }
